7486dd0e8827df85cde991d2841a5a0592716633
mpralat <pralatmarta@gmail.com>
2017-03-01 16:06:04 +0100

    Reading file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("test_image.png"));
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+        }
+        System.out.println(img);
     }
 }
