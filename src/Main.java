a8c6619c40b3c0e9b62d48115a8bb41f8ce8769a
mpralat <pralatmarta@gmail.com>
2017-03-25 19:36:08 +0100

    Fix GUI layout and add ComputationManager file

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1250, 520));
+        primaryStage.setTitle("Tomograph");
+        primaryStage.setScene(new Scene(root, 1250, 480));
         primaryStage.setResizable(false);
         primaryStage.show();
 
